b37880f51fc3d82950dde8f81bcdf56352009752
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-12 15:48:56 -0400

    initial commit

---
+++ providers.tf
@@ -0,0 +1,33 @@
+terraform {
+  cloud {
+    organization = "aalok-trivedi"
+    workspaces {
+      name = "ci_cd_testing_dev"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      App         = var.app_name
+      Environment = var.environment
+      Terraform   = "True"
+    }
+  }
+}
+
+# Retrieve the list of AZs in the current AWS region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
